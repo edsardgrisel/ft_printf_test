0a53b937e1ab8f393a5fe546121d774433b1ac9e
edsardgrisel <edsardgrisel1@gmail.com>
2025-05-08 17:04:43 +0200

    header:

---
+++ test.h
@@ -0,0 +1,16 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../printf_repo/include/libftprintf.h"
+
+
+void	test_char();
+void	test_no_conversion();
+void	test_string();
+void	test_num_hex();
+
+
+
